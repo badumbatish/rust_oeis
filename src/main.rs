41a8261df75b93c56c2a753d7d029e0a581926a1
Jasmine T <tanghocle456@gmail.com>
2023-07-18 21:57:40 -0500

    General idea achieved

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use std::collections::HashMap;
 
+use tokio;
 use clap::{Parser, Subcommand};
+use reqwest;
+use serde_json;
+
+
 
 /// Program to analyze integer sequence
 #[derive(Parser, Debug)]
-#[command(author = "Jasmine Tang", version, about, long_about = None )]
+#[command(author = "Jasmine Tang", version, about, long_about = None)]
 struct CLI {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    query: String,
 
 
     #[command(subcommand)]
@@ -18,18 +21,24 @@ struct CLI {
 #[derive(Subcommand)]
 enum Commands {
     
-
-   Query { input: Option<String> },
+    /// Query a sequence of number from OEIS
+    Query { input: Option<String> },
 }
 
-fn main() {
+
+#[tokio::main]
+async fn main() -> Result<(), Box< dyn std::error::Error>> {
     let args = CLI::parse();
     
-
     match &args.command {
         Commands::Query { input} => {
-            println!("{input:?}:")
+            let res = reqwest::get("https://oeis.org/search?q={input}&fmt=json")
+                        .await?
+                        .json::<serde_json::Value>()
+                        .await?;
+            println!("{}", serde_json::to_string_pretty(&res).unwrap());
         }
-    }
-
+    };
+    
+    Ok(())
 } 
