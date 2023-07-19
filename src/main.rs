245ed0ef779928c9614522fc4fa8e6f258e6b2af
Jasmine T <tanghocle456@gmail.com>
2023-07-18 20:05:54 -0500

    We getting there

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+
+use clap::{Parser, Subcommand};
+
+/// Program to analyze integer sequence
+#[derive(Parser, Debug)]
+#[command(author = "Jasmine Tang", version, about, long_about = None )]
+struct CLI {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    query: String,
+
+
+    #[command(subcommand)]
+    command : Commands,
 }
+
+#[derive(Debug)]
+#[derive(Subcommand)]
+enum Commands {
+    
+
+   Query { input: Option<String> },
+}
+
+fn main() {
+    let args = CLI::parse();
+    
+
+    match &args.command {
+        Commands::Query { input} => {
+            println!("{input:?}:")
+        }
+    }
+
+} 
